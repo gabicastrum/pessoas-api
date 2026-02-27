f21a7977b0ce8478b588103a11fa82be568174c1
Gabriela de Castro Laurindo <gabriela.laurindo@dbserver.com.br>
2026-02-27 12:37:57 -0300

    feat(TASK-01): definido o contrato de request com dto

---
+++ src/main/java/com/gestao/pessoas/dto/request/EnderecoRequestDTO.java
@@ -0,0 +1,11 @@
+package com.gestao.pessoas.dto.request;
+
+public record EnderecoRequestDTO(
+        String rua,
+        String numero,
+        String bairro,
+        String cidade,
+        String estado,
+        String cep
+) {
+}
