56dbe253f086ab6e3228a733f9e11258524cb740
Gabriela de Castro Laurindo <gabriela.laurindo@dbserver.com.br>
2026-02-27 12:03:24 -0300

    feat(TASK-01): criado entidade endereco

---
+++ src/main/java/com/gestao/pessoas/domain/Endereco.java
@@ -0,0 +1,34 @@
+package com.gestao.pessoas.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "enderecos")
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String rua;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+
+    @NotBlank
+    private String cep;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pessoa_id", nullable = false)
+    private Pessoa pessoa;
+}
