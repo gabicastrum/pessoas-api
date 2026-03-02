9f3fb859031d16c862bba979dccdb424967ed3bb
Gabriela de Castro Laurindo <gabriela.laurindo@dbserver.com.br>
2026-03-02 17:20:48 -0300

    refactor(TASK-01): refatorado ala implementaçcao de cadastro

--- src/main/java/com/gestao/pessoas/service/PessoaService.java
+++ src/main/java/com/gestao/pessoas/service/PessoaService.java
@@ -6,37 +6,27 @@ import com.gestao.pessoas.exception.CpfExisteException;
 import com.gestao.pessoas.mapper.PessoaMapper;
 import com.gestao.pessoas.repository.PessoaRepository;
 import jakarta.transaction.Transactional;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor //cria o construtor para injeção de automatica
+@RequiredArgsConstructor
 public class PessoaService {
-    //pq private final?
-    //pq o repository e o mapper
     private final PessoaRepository pessoaRepository;
     private final PessoaMapper pessoaMapper;
 
-    //pq o uso de void?
-    // pq RuntimeException e não outro?
     @Transactional
     public void cadastrarPessoa(PessoaRequestDTO dto) {
         if (pessoaRepository.existsByCpf(dto.cpf())) {
             throw new CpfExisteException();
         }
 
-        //convertendo DTO para a entidade
         Pessoa pessoa = pessoaMapper.toEntity(dto);
 
-        //Conectando
         if (pessoa.getEnderecos() != null) {
-            //para cada endereco da lista...
             pessoa.getEnderecos().forEach(endereco->
-                    //dizemos: ei, sua pessoa está aqui endereco
                     endereco.setPessoa(pessoa));
         }
-        //e então salvamos
         pessoaRepository.save(pessoa);
     }
 
