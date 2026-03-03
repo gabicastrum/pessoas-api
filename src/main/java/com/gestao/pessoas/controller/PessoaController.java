f7f27992f77de472a296abc2a710cc1deadf5b56
Gabriela de Castro Laurindo <gabriela.laurindo@dbserver.com.br>
2026-03-03 10:52:39 -0300

    feat(TASK-03): implementando o endpoint para listar pessoas e seus enderecos

--- src/main/java/com/gestao/pessoas/controller/PessoaController.java
+++ src/main/java/com/gestao/pessoas/controller/PessoaController.java
@@ -1,12 +1,16 @@
 package com.gestao.pessoas.controller;
 
+import com.gestao.pessoas.domain.Pessoa;
 import com.gestao.pessoas.dto.request.EnderecoRequestDTO;
 import com.gestao.pessoas.dto.request.PessoaRequestDTO;
+import com.gestao.pessoas.dto.response.PessoaResponseDTO;
 import com.gestao.pessoas.service.PessoaService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -29,4 +33,9 @@ public class PessoaController {
         service.adicionarEndereco(idPessoa, dto);
     }
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<PessoaResponseDTO> listarPessoas() {
+        return service.listarPessoas();
+    }
 }
