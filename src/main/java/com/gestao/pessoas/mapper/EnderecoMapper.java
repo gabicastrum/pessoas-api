2f1a84e16648e56f60c64a81ba26d0cf1d371adb
Gabriela de Castro Laurindo <gabriela.laurindo@dbserver.com.br>
2026-03-04 12:47:18 -0300

    refactor(TASK-07): adicionando retorno de erros para criar novos enderecos

--- src/main/java/com/gestao/pessoas/mapper/EnderecoMapper.java
+++ src/main/java/com/gestao/pessoas/mapper/EnderecoMapper.java
@@ -2,6 +2,8 @@ package com.gestao.pessoas.mapper;
 
 import com.gestao.pessoas.domain.Endereco;
 import com.gestao.pessoas.dto.request.EnderecoRequestDTO;
+import com.gestao.pessoas.dto.response.EnderecoResponseDTO;
+import com.gestao.pessoas.dto.response.EnderecoResultadoDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -13,4 +15,6 @@ public interface EnderecoMapper {
     Endereco toEntity(EnderecoRequestDTO dto);
 
     EnderecoRequestDTO toDto(Endereco endereco);
+
+    EnderecoResponseDTO toDTO(Endereco endereco);
 }
