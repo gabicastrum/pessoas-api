732d487935c408fe89a8b2cf6ec9905c75c79fe1
Gabriela de Castro Laurindo <gabriela.laurindo@dbserver.com.br>
2026-03-04 12:47:18 -0300

    refactor(TASK-07): adicionando retorno de erros para criar novos enderecos

--- src/main/java/com/gestao/pessoas/repository/PessoaRepository.java
+++ src/main/java/com/gestao/pessoas/repository/PessoaRepository.java
@@ -2,11 +2,18 @@ package com.gestao.pessoas.repository;
 
 import com.gestao.pessoas.domain.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 
     //SELECT COUNT(*) FROM pessoas WHERE cpf = ?
     boolean existsByCpf(String cpf);
+
+    @Query("SELECT p FROM Pessoa p JOIN FETCH p.enderecos WHERE p.id = :id")
+    Optional<Pessoa> findByIdComEnderecos(@Param("id") Long id);
 }
